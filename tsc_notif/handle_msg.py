51c9f72a95a9f70ba6a4a37c77f199a34daab585
aitsc <tsc@diy>
2024-07-22 22:07:21 +0800

    chore: Update handle_msg.py to use JSON serialization for message content and add eventname search functionality

--- tsc_notif/handle_msg.py
+++ tsc_notif/handle_msg.py
@@ -1,4 +1,5 @@
 import json
+import re
 from .utils import push_msg, PushPlusSend
 from .supervisor_eventlistener import print
 
@@ -8,18 +9,26 @@ def msg_callback(
     context: dict = None,
     token: str = '',
     title: str = 'supervisor',
+    search_eventname: str = r'^(PROCESS_STATE_FATAL|PROCESS_STATE_EXITED|PROCESS_COMMUNICATION)',
     **kwargs,
 ):
-    if msg['headers'].get('eventname') in {
-        'PROCESS_STATE_FATAL',
-        'PROCESS_STATE_EXITED',
-        'PROCESS_COMMUNICATION',
-    }:
+    """消息回调的例子
+
+    Args:
+        msg (dict): 消息
+        context (dict, optional): 上下文
+        token (str, optional): pushpush 用户令牌
+        title (str, optional): 消息标题
+        search_eventname (str, optional): 匹配 eventname 的正则表达式
+            PROCESS_STATE,PROCESS_LOG,PROCESS_COMMUNICATION,SUPERVISOR_STATE_CHANGE,PROCESS_GROUP,TICK
+    """
+    if re.search(search_eventname, msg['headers']['eventname']):
         m = PushPlusSend(
             token=token,
             title=title,
             content=json.dumps(msg),
             template='json',
+            **kwargs,
         )
         print('push_msg:', push_msg(m))
 
